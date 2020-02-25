286f1619750d9f8f39c570a9d9bc8c2d6353a221
JefersonUrbieta <jeferson.urbieta@gmail.com>
2020-02-25 04:13:21 +0000

    Deploying to gh-pages from  - 01a0d76c9e44f7f442ce9b1d348fe406eedc72c7 🚀

---
+++ precache-manifest.fa32f8cb337da292220f68c180a0208a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0733369a96d063834a0d",
+    "url": "/bin2dec/css/app.41e55b0d.css"
+  },
+  {
+    "revision": "c268a61b2fac87acfe46",
+    "url": "/bin2dec/css/chunk-vendors.920135ba.css"
+  },
+  {
+    "revision": "d77389d297bc7493739e2d76e04172b9",
+    "url": "/bin2dec/favicon-16x16.png"
+  },
+  {
+    "revision": "1014ccf3e25524343fd72a3e4b0c2aeb",
+    "url": "/bin2dec/favicon-32x32.png"
+  },
+  {
+    "revision": "40c52418356cf5f7973964d5283ed530",
+    "url": "/bin2dec/img/logo.40c52418.svg"
+  },
+  {
+    "revision": "ba13dfe5a01b48b92c8dfe1bf188fa71",
+    "url": "/bin2dec/index.html"
+  },
+  {
+    "revision": "0733369a96d063834a0d",
+    "url": "/bin2dec/js/app.649bd4de.js"
+  },
+  {
+    "revision": "c268a61b2fac87acfe46",
+    "url": "/bin2dec/js/chunk-vendors.c7fce3b7.js"
+  },
+  {
+    "revision": "4b653b8816dc1422a5ef0d8646c804d5",
+    "url": "/bin2dec/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bin2dec/robots.txt"
+  }
+]);
